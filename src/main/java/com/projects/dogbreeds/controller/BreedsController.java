951311dfa79dd9b6eed081d4fa38abab926b75c6
Justyna <justyna.slubowska@interia.pl>
2023-03-03 20:20:00 +0100

    Add methods "update" with http methods Put and Patch Add method "response if breed is unknown"

--- src/main/java/com/projects/dogbreeds/controller/BreedsController.java
+++ src/main/java/com/projects/dogbreeds/controller/BreedsController.java
@@ -2,6 +2,7 @@ package com.projects.dogbreeds.controller;
 
 import com.projects.dogbreeds.model.Breed;
 import com.projects.dogbreeds.repository.BreedsRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.List;
 public class BreedsController {
 
     @GetMapping("all")
-    public List<Breed> getAll() {
+    public List<Breed> getAll()
+    {
         return BreedsRepository.breedsRepository;
     }
 
@@ -20,4 +22,63 @@ public class BreedsController {
         BreedsRepository.breedsRepository.add(breed);
         return breed;
     }
+    @PutMapping("update")
+    public String updateWithFullBody (@RequestBody Breed breed, HttpServletResponse response) {
+        if (BreedsRepository.breedsRepository.size() - 1 < breed.getId()) {
+            return getResponseUnknownBreed(response);
+        } else {
+            BreedsRepository.breedsRepository.set(breed.getId(), breed);
+        }
+        return "Breed " + breed.getName() + " is updated.";
+    }
+
+    //    private String origin;
+
+    @PatchMapping("update")
+    public String updatePartial(@RequestBody Breed breed, HttpServletResponse response) {
+        if (BreedsRepository.breedsRepository.size() - 1 < breed.getId()) {
+            return getResponseUnknownBreed(response);
+        } else {
+
+            Breed breedInRepo = BreedsRepository.breedsRepository.get(breed.getId());
+
+            if (breed.getName() != null) {
+                breedInRepo.setName(breed.getName());
+            }
+            if (breed.getSize() != null) {
+                breedInRepo.setSize(breed.getSize());
+            }
+            if (breed.getMaleWeight() != null) {
+                breedInRepo.setMaleWeight(breed.getMaleWeight());
+            }
+            if (breed.getFemaleWeight() != null) {
+                breedInRepo.setFemaleWeight(breed.getFemaleWeight());
+            }
+            if (breed.getLowerAverageAge() != null) {
+                breedInRepo.setLowerAverageAge(breed.getLowerAverageAge());
+            }
+            if (breed.getHigherAverageAge() != null) {
+                breedInRepo.setHigherAverageAge(breed.getHigherAverageAge());
+            }
+            if (breed.getCharacterTraits() != null) {
+                breedInRepo.setCharacterTraits(breed.getCharacterTraits());
+            }
+            if (breed.getCoatType() != null) {
+                breedInRepo.setCoatType(breed.getCoatType());
+            }
+            if (breed.getColorCoat() != null) {
+                breedInRepo.setColorCoat(breed.getColorCoat());
+            }
+            if (breed.getOrigin() != null) {
+                breedInRepo.setOrigin(breed.getOrigin());
+            }
+
+            return "Breed " + breedInRepo.getName() + " is partially updated.";
+        }
+    }
+    private static String getResponseUnknownBreed(HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        return "Breed is not recognized.Try to add a new breed.";
+    }
+
 }
